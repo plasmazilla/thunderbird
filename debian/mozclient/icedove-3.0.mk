4fe1a7acf7012a875604119e64d5f5671a7bf9e4
Christoph Goehre <chris@sigxcpu.org>
2009-11-25 18:43:31 +0100

    add debian packaging

---
+++ debian/mozclient/icedove-3.0.mk
@@ -0,0 +1,32 @@
+# -*- mode: makefile; coding: utf-8 -*-
+
+# Copyright (c) 2009 Guido Guenther <agx@sigxcpu.org>
+# Description: Project Icedove 3.0
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+BRANDING_DIR = build-tree/mozilla/mail/branding/icedove/
+
+MOZCLIENT_PROJECTNAME := icedove-3.0
+include debian/mozclient/thunderbird-3.0.mk
+
+post-patches:: debian/stamp-icedove-branding
+debian/stamp-icedove-branding:
+	cp -af debian/icedove-branding $(BRANDING_DIR)
+	for uue in `find $(BRANDING_DIR) -name '*.uu'`; do \
+		uudecode $$uue; rm $$uue; \
+	done
+	touch $@
+
