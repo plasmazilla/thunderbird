a21efa9c04e621a90123f3bfda9d91cffad6a550
Christoph Goehre <chris@sigxcpu.org>
2011-08-03 17:21:03 +0200

    add branding for icedove 5.0

--- debian/icedove-branding/thunderbird-branding.js
+++ debian/icedove-branding/thunderbird-branding.js
@@ -9,3 +9,5 @@ pref("mailnews.start_page.welcome_url", "about:");
 // start page override to load after an update
 //pref("mailnews.start_page.override_url", "chrome://messenger/content/start.xhtml");
 pref("mailnews.start_page.override_url", "about:");
+
+pref("app.vendorURL", "http://www.mozillamessaging.com/%LOCALE%/thunderbird/");
