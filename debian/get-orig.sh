b1b1d9caf17d5e350b066c84ff169b7e6ef3a138
Christoph Goehre <chris@sigxcpu.org>
2013-01-02 18:49:33 -0500

    changing download url for xpi files
    mirror.switch.ch not longer supporting mirroring mozilla files
    
    this is cherry-picked from 19d192ace7214409762244815987975298686aa8

--- debian/get-orig.sh
+++ debian/get-orig.sh
@@ -19,16 +19,17 @@ if [ -f $CURDIR/../icedove-l10n_$VERSION.orig.tar.xz ]; then
     exit 1
 fi
 
-ALLXPI=`wget http://mirror.switch.ch/ftp/mirror/mozilla/thunderbird/releases/$VERSION/linux-i686/xpi -O - | grep ".xpi</a>" | awk -F\" '{ print $10 }'`
-
 mkdir $TMPDIR/icedove-l10n-$VERSION
 mkdir $TMPDIR/icedove-l10n-$VERSION/upstream
 cd $TMPDIR/icedove-l10n-$VERSION
 
-for XPI in $ALLXPI; do
+# use wget mirror mode
+wget -m ftp://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/$VERSION/linux-i686/xpi
+cp ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/$VERSION/linux-i686/xpi/*.xpi upstream
+
+for XPI in `ls upstream`; do
     LOCALE=`basename $XPI .xpi`
     mkdir upstream/$LOCALE
-    wget -O upstream/$XPI -4 http://mirror.switch.ch/ftp/mirror/mozilla/thunderbird/releases/$VERSION/linux-i686/xpi/$XPI
     unzip -o -q -d upstream/$LOCALE upstream/$XPI
     cd upstream/$LOCALE
     if [ -f chrome/$LOCALE.jar ]; then
@@ -47,6 +48,7 @@ done
 # en-US is integrated in icedove itself
 cd $TMPDIR/icedove-l10n-$VERSION
 rm -rf upstream/en-US
+rm -rf ftp.mozilla.org
 cd ..
 tar -Jcf icedove-l10n_$VERSION.orig.tar.xz icedove-l10n-$VERSION
 cp icedove-l10n_$VERSION.orig.tar.xz $CURDIR/..
