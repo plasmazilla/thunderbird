a0b20e7fd2f3b507c00ff34b3c372af29b6d0723
Carsten Schoenert <c.schoenert@t-online.de>
2016-06-04 21:53:23 +0200

    debian/tests/*: adopt change of the binary icedove
    With the change of the binary name from
    
     /usr/lib/icedove/icedove ->
     /usr/lib/thunderbird/thunderbird
    
    (and the linking to the binary) we need to adjust the autopkg tests as
    well.

--- debian/tests/help.sh
+++ debian/tests/help.sh
@@ -5,9 +5,9 @@ set -e
 # At least check we can execute the main binary
 # to catch missing dependenies
 echo -n "Test1: checking help output..."
-xvfb-run -a icedove -help >/dev/null
+xvfb-run -a thunderbird -help >/dev/null
 echo "done."
 
 echo -n "Test2: checking version output..."
-xvfb-run -a icedove --version | grep -qs Icedove
+xvfb-run -a thunderbird --version | grep -qs Thunderbird
 echo "done."
--- debian/tests/idlTest.sh
+++ debian/tests/idlTest.sh
@@ -15,22 +15,22 @@ TESTFILE=$(basename $0 .sh)
 echo "Running tests in ${TESTFILE}"
 
 echo -n "Test1: Make sure we can generate typelibs..."
-/usr/lib/icedove-devel/sdk/bin/typelib.py \
-    -I"/usr/lib/icedove-devel/idl" \
+/usr/lib/thunderbird-devel/sdk/bin/typelib.py \
+    -I"/usr/lib/thunderbird-devel/idl" \
     -o "${SCRATCHDIR}/test.xpt" \
     "${TEST_IDL_FILE}"
 echo "done."
 
 echo -n "Test2: Make sure we can generate C++ headers..."
-/usr/lib/icedove-devel/sdk/bin/header.py \
-    -I"/usr/lib/icedove-devel/idl" \
+/usr/lib/thunderbird-devel/sdk/bin/header.py \
+    -I"/usr/lib/thunderbird-devel/idl" \
     -o "${SCRATCHDIR}/test.h" \
     "${TEST_IDL_FILE}"
 echo "done."
 
 echo -n "Test3: Compiling generated file..."
 g++ -std=c++11 \
-    -I/usr/include/icedove \
+    -I/usr/include/thunderbird \
     -I/usr/include/nspr    \
     -o "${SCRATCHDIR}/test.o" \
     "${SCRATCHDIR}/test.h"
