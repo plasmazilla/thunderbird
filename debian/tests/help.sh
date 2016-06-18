a03329c887b586752de7d5e2542c3d984695794f
Carsten Schoenert <c.schoenert@t-online.de>
2016-06-18 10:09:25 +0200

    debian/tests/help.sh: use absolute path for binary call
    After we use a wrapper script as /usr/bin/thunderbird we need to call
    the main binary in /usr/lib/thunderbird/thunderbird to check the xvfb-run call.

--- debian/tests/help.sh
+++ debian/tests/help.sh
@@ -5,9 +5,9 @@ set -e
 # At least check we can execute the main binary
 # to catch missing dependenies
 echo -n "Test1: checking help output..."
-xvfb-run -a thunderbird -help >/dev/null
+xvfb-run -a /usr/lib/thunderbird/thunderbird -help >/dev/null
 echo "done."
 
 echo -n "Test2: checking version output..."
-xvfb-run -a thunderbird --version | grep -qs Thunderbird
+xvfb-run -a /usr/lib/thunderbird/thunderbird --version | grep -qs Thunderbird
 echo "done."
